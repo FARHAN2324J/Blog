24b2d848ec2905ef0444f7bb65ca334831f9fa8b
Farhan <farhanfadayi@gmai.com>
2026-02-05 10:26:13 +0330

    updated not-found

--- app/[id]/not-found.tsx
+++ app/[id]/not-found.tsx
@@ -1,44 +1,3 @@
-// const NotFound = () => {
-//   return (
-//     <div className="bg-black text-red-800">NotFound</div>
-//   )
-// }
-
-// export default NotFound
-
-// "use client";
-
-// import { usePathname } from "next/navigation";
-
-// const Notfound = () => {
-//   const pathname = usePathname();
-//   const productId = pathname.split("/")[2];
-//   const reviewId = pathname.split("/")[4];
-//   return (
-//     <div className="text-white">
-//       review {reviewId} Notfound for {productId}
-//     </div>
-//   );
-// };
-
-// export default Notfound;
-
-// "use client";
-
-// import { useParams } from "next/navigation";
-
-// const Notfound = () => {
-//   const params = useParams();
-
-//   // اگر مسیرت مثلاً `/4` باشه، params = { id: "4" }
-//   const id = params.id as string; // "4"
-
-//   return <div className="text-white">Page with ID {id} not found</div>;
-// };
-
-// export default Notfound;
-
-
 "use client";
 
 import Link from "next/link";
@@ -46,33 +5,28 @@ import { useParams } from "next/navigation";
 
 const Notfound = () => {
   const params = useParams();
-  const postId = params.id as string; // اینجا "id" همون پارامتر داینامیک مسیرت هست
-  
+  const postId = params.id as string;
   return (
-    <div className="min-h-screen flex items-center justify-center bg-(--primary-color)">
-      <div className="text-center p-8 rounded-2xl bg-(--secondary-color)">
-        <h1 className="text-4xl font-bold text-(--text-white) mb-4">
-          پست پیدا نشد! 🚫
+    <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-xl font-semibold text-(--primary-color)">404</p>
+        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-(--text-white) sm:text-7xl">
+          Page not found
         </h1>
-        <p className="text-(--text-gray) text-lg mb-6">
-          متاسفانه پستی با شناسه 
-          <span className="text-orange-500 font-mono mx-2">#{postId}</span>
-          وجود ندارد.
+        <p className="my-6 text-lg font-medium text-pretty text-(--text-gray) sm:text-xl/8">
+          There is no postal information with ID <span className="text-(--primary-color)">#{postId}</span>
         </p>
-        <div className="space-y-4">
-          <Link 
-            href="/"
-            className="inline-block px-6 py-3 bg-orange-500 text-white rounded-full hover:bg-orange-600 transition"
-          >
-            بازگشت به وبلاگ
-          </Link>
-          <p className="text-(--text-light-gray) text-sm">
-            یا یک شناسه معتبر وارد کنید
-          </p>
-        </div>
+        <Link
+          href="/"
+          className="rounded-md bg-(--primary-color) px-3.5 py-2.5  text-sm font-semibold text-(--text-white)"
+        >
+          Go back home
+        </Link>
       </div>
-    </div>
+    </main>
   );
 };
 
 export default Notfound;
+
+//
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
+
 const NotFound = () => {
   return (
-    <div className="bg-white text-black">NotFound</div>
-  )
-}
+    <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-xl font-semibold text-(--primary-color)">404</p>
+        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-(--text-white) sm:text-7xl">
+          Page not found
+        </h1>
+        <p className="my-6 text-lg font-medium text-pretty text-(--text-gray) sm:text-xl/8">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <Link
+          href="/"
+          className="rounded-md bg-(--primary-color) px-3.5 py-2.5  text-sm font-semibold text-(--text-white)"
+        >
+          Go back home
+        </Link>
+      </div>
+    </main>
+  );
+};
 
-export default NotFound
+export default NotFound;
