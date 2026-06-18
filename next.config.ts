b015fe664ef52abe83af366612b5a9e3fa078c3c
Farhan <farhanfadayi@gmai.com>
2026-06-18 11:19:32 +0430

    edited next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,20 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ["i.imgur.com"],
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "assets.example.com",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -8,6 +25,11 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "assets.example.com",
+      },
+      {
+        protocol: "https",
+        hostname: "zfjvutdwaalcjqhnoezd.supabase.co",
+        pathname: "/storage/v1/object/public/**",
       },
     ],
   },
